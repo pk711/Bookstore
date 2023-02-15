af5c30f98e5d713c6055d7117fb278c52db5f2b4
pk711 <114118300+pk711@users.noreply.github.com>
2023-02-15 08:44:57 +0000

    Delete frog_task_bookstore directory

--- frog_task_bookstore/src/main/java/frog/task/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package frog.task.repository;
-
-import frog.task.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
